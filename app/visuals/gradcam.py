ed6c1936d88d1451348e87e4a7b8a4eb2f02aaa4
dhruv <dhruvbhaikagathara@gamil.com>
2026-01-21 19:48:26 +0530

    finally

--- app/visuals/gradcam.py
+++ app/visuals/gradcam.py
@@ -6,34 +6,15 @@ from pytorch_grad_cam.utils.image import show_cam_on_image
 import cv2
 
 def generate_heatmap(model, input_tensor, original_image):
-    
+
     
     target_layers = [model.rgb_branch.features[-1]]
-
-  
     cam = GradCAM(model=model, target_layers=target_layers)
-
     targets = [ClassifierOutputTarget(1)]
-    
-    
     dummy_freq = torch.zeros_like(input_tensor) 
-    
-    
-    
     grayscale_cam = cam(input_tensor=input_tensor, targets=targets)[0, :]
-    
-   
     rgb_img = np.float32(original_image) / 255.0
     rgb_img = cv2.resize(rgb_img, (256, 256))
-    
     visualization = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
-    
     return visualization
-    class RGBWrapper(torch.nn.Module):
-        def __init__(self, full_model):
-            super().__init__()
-            self.main = full_model.rgb_branch 
-        def forward(self, x):
-            return self.main(x)
-
-cam = GradCAM(model=RGBWrapper(model), target_layers=[model.rgb_branch.features[-1]])
+    
